0a72104c5f0176c075e87ab1a4e20e70de0bbc98
DANIEL REMOQUILLO <remoquillodr@gmail.com>
2023-02-21 14:54:58 +0800

    ui design

---
+++ lib/screens/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //View type
+  double _zoom = 200.0;
+
+  _zoomOut() {
+    setState(() {
+      if (_zoom <= 200) {
+        _zoom -= 50.0;
+        if (_zoom <= 100) {
+          _zoom = 100.0;
+        }
+      } else {
+        _zoom -= 200.0;
+      }
+    });
+  }
+
+  _zoomIn() {
+    setState(() {
+      if (_zoom >= 400) {
+        _zoom = 400.0;
+      } else {
+        if (_zoom == 200.0) {
+          _zoom += 200.0;
+        } else {
+          _zoom += 100.0;
+        }
+      }
+    });
+  }
+
+  List<Widget> imageFiles = [
+    Container(height: 200, width: 200, color: Colors.amber),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gallery'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                _zoomIn();
+                print(_zoom);
+              },
+              icon: Icon(
+                Icons.zoom_in,
+                color: _zoom != 400.0 ? Colors.white : Colors.grey,
+              )),
+          IconButton(
+              onPressed: () {
+                _zoomOut();
+                print(_zoom);
+              },
+              icon: Icon(
+                Icons.zoom_out,
+                color: _zoom != 100.0 ? Colors.white : Colors.grey,
+              )),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return imageFiles[0];
+                  },
+                  childCount: 200,
+                ),
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: _zoom,
+                    mainAxisSpacing: 2.0,
+                    crossAxisSpacing: 2.0))
+          ],
+        ),
+      ),
+    );
+  }
+}
